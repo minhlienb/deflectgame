7fbfeaeec2e6ca7e6943fd7432d93d0dddfcdc6d
Lien Bui Minh <minhlien2077@gmail.com>
2025-06-06 00:00:19 +0700

    ngol

--- server.js
+++ server.js
@@ -1,13 +1,69 @@
 const WebSocket = require('ws');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-// Create a WebSocket server on port 8080
-const wss = new WebSocket.Server({ port: 8080 });
+// Create HTTP server
+const server = http.createServer((req, res) => {
+    // Get the file path
+    let filePath = path.join(__dirname, req.url === '/' ? 'main.html' : req.url);
+    
+    // Get the file extension
+    const extname = path.extname(filePath);
+    
+    // Set content type based on file extension
+    let contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+    
+    // Read the file
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                // Page not found
+                fs.readFile(path.join(__dirname, '404.html'), (err, content) => {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf-8');
+                });
+            } else {
+                // Server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            // Success
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+// Create a WebSocket server using the HTTP server
+const wss = new WebSocket.Server({ server });
 
 // Store connected clients
 const clients = new Map();
 let nextPlayerId = 1;
 
-console.log('WebSocket server started on port 8080');
+// Start the server
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
 
 wss.on('connection', (ws) => {
     // Assign a unique ID to this client
